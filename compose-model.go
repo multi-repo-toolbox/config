e59da4d4ea8fb82e62d2322ae9a8fa60b8e4f9ff
Alexander I.Grafov <grafov@gmail.com>
2021-04-12 00:50:26 +0300

    Add comments

--- compose-model.go
+++ compose-model.go
@@ -1,8 +1,11 @@
 package config
 
+// Compose keeps configuration for docker-compose.
 type Compose struct {
 	Services map[string]ComposeService
 }
+
+// ComposeService keeps configuration for the single service.
 type ComposeService struct {
 	Repo    string       `yaml:"remote"`  // remote repository
 	Image   string       `yaml:"image"`   // use docker image instead
@@ -10,6 +13,8 @@ type ComposeService struct {
 	Command string       `yaml:"command"` // path to a command with optional args for run a service
 	Build   ComposeBuild `yaml:"build"`   // compatible with docker-compose
 }
+
+// ComposeBuild keeps configuration for the build recipes.
 type ComposeBuild struct {
 	Context    string `yaml:"context"`    // path to build context
 	Dockerfile string `yaml:"dockerfile"` // path to alternate dockerfile, used when build command empty
--- extended-model.go
+++ extended-model.go
@@ -1,13 +1,20 @@
 package config
 
+// Extended keeps full configuration for MRT that includes
+// fields for the docker-compose.
 type Extended struct {
 	Services map[string]Service
 }
 
+// Service keeps configuration for the single service. It based on
+// docker-compose Service.
 type Service struct {
 	ComposeService
 	Base string `yaml:"base"` // base path to the service
 }
+
+// Build keeps configuration for build recipes. It based on
+// docker-compose Build.
 type Build struct {
 	ComposeBuild
 	Cmd string `yaml:"command"` // path to build command that run in context
